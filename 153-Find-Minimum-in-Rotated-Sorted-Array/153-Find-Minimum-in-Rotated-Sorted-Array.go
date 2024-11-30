00afeb7b69c4a70adaebd90080e1c0814e25f6df
silentFellow <gowtham.sri+github@zohomail.in>
2024-11-30 12:18:09 +0530

    Add new files in 153-Find-Minimum-in-Rotated-Sorted-Array

---
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/153-Find-Minimum-in-Rotated-Sorted-Array.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func findMin(nums []int) int {
+	l, r := 0, len(nums)-1
+	for l < r {
+		m := (l + r) / 2
+
+		if nums[m] > nums[r] {
+			l = m + 1
+		} else {
+			r = m
+		}
+	}
+
+	return nums[l]
+}
---
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/153-Find-Minimum-in-Rotated-Sorted-Array_test.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import "testing"
+
+func Test_findMin(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "rotated 3 times",
+			args: args{nums: []int{3, 4, 5, 1, 2}},
+			want: 1,
+		},
+		{
+			name: "rotated 4 times",
+			args: args{nums: []int{4, 5, 6, 7, 0, 1, 2}},
+			want: 0,
+		},
+		{
+			name: "not rotated",
+			args: args{nums: []int{11, 13, 15, 17}},
+			want: 11,
+		},
+		{
+			name: "rotated once",
+			args: args{nums: []int{2, 1}},
+			want: 1,
+		},
+		{
+			name: "rotated n-1 times",
+			args: args{nums: []int{1, 2, 3, 4, 5}},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMin(tt.args.nums); got != tt.want {
+				t.Errorf("findMin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
