2cdae877c64abb03ada1bb2ec70f40cd235c110a
silentFellow <gowtham.sri+github@zohomail.in>
2024-12-30 19:10:06 +0530

    Update files in 26-Remove-Duplicates-from-Sorted-Array

--- 26-Remove-Duplicates-from-Sorted-Array/26-Remove-Duplicates-from-Sorted-Array.go
+++ 26-Remove-Duplicates-from-Sorted-Array/26-Remove-Duplicates-from-Sorted-Array.go
@@ -1,17 +1,11 @@
 package leetcode
 
 func removeDuplicates(nums []int) int {
-	k, prev := 1, nums[0]
+	k := 1
 
-	r := 1
-	for r < len(nums) {
-		for r < len(nums) && nums[r] == prev {
-			r++
-		}
-
-		if r < len(nums) {
-			prev = nums[r]
-			nums[k] = nums[r]
+	for i:=1; i<len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
 			k++
 		}
 	}
