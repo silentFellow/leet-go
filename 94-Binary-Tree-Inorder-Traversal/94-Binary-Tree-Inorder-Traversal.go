a778722e4f3302a1b4883997716465cff7dc5b8b
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-03 22:54:10 +0530

    Update files in 94-Binary-Tree-Inorder-Traversal

--- 94-Binary-Tree-Inorder-Traversal/94-Binary-Tree-Inorder-Traversal.go
+++ 94-Binary-Tree-Inorder-Traversal/94-Binary-Tree-Inorder-Traversal.go
@@ -1,7 +1,5 @@
 package leetcode
 
-// TODO: Implement the function inorderTraversal without recurrsion
-
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -9,15 +7,36 @@ type TreeNode struct {
 }
 
 func inorderTraversal(root *TreeNode) []int {
-	if root == nil {
-		return nil
+	stack := []*TreeNode{}
+	result := []int{}
+	current := root
+
+	for current != nil || len(stack) != 0 {
+		for current != nil {
+			stack = append(stack, current)
+			current = current.Left
+		}
+
+		current = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		result = append(result, current.Val)
+
+		current = current.Right
 	}
 
-	nums := make([]int, 0)
-
-	nums = append(nums, inorderTraversal(root.Left)...)
-	nums = append(nums, root.Val)
-	nums = append(nums, inorderTraversal(root.Right)...)
-
-	return nums
+	return result
 }
+
+// func inorderTraversal(root *TreeNode) []int {
+// 	if root == nil {
+// 		return nil
+// 	}
+//
+// 	nums := make([]int, 0)
+//
+// 	nums = append(nums, inorderTraversal(root.Left)...)
+// 	nums = append(nums, root.Val)
+// 	nums = append(nums, inorderTraversal(root.Right)...)
+//
+// 	return nums
+// }
