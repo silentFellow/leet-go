2c534692fb0e04b130ef95de2bfc385376344f73
silentFellow <gowtham.sri+github@zohomail.in>
2025-02-15 07:40:33 +0530

    Update files in 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits

--- 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits/2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.go
+++ 2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits/2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.go
@@ -1,49 +1,19 @@
 package leetcode
 
-import "container/heap"
-
 func maximumSum(nums []int) int {
-	hmap := make(map[int]*HeapStruct)
+	hmap := make(map[int]int)
 
 	ans := -1
 	for _, val := range nums {
 		sum := sumOfDigit(val)
-		if _, ok := hmap[sum]; ok {
-			maxv := (*hmap[sum])[0]
-			ans = max(ans, val+maxv)
+		if v, ok := hmap[sum]; ok {
+			ans = max(ans, val+v)
 		}
 
-		if _, ok := hmap[sum]; !ok {
-			hmap[sum] = &HeapStruct{}
-			heap.Init(hmap[sum])
-		}
-		heap.Push(hmap[sum], val)
+		hmap[sum] = max(hmap[sum], val)
 	}
 
 	return ans
-}
-
-// heap structure
-type HeapStruct []int
-
-// sort interfece
-func (h HeapStruct) Len() int { return len(h) }
-
-func (h HeapStruct) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-
-func (h HeapStruct) Less(i, j int) bool { return h[i] > h[j] }
-
-// heap interfece
-func (h *HeapStruct) Push(v any) {
-	*h = append(*h, v.(int))
-}
-
-func (h *HeapStruct) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[:n-1]
-	return x
 }
 
 // calculate sum of digits
