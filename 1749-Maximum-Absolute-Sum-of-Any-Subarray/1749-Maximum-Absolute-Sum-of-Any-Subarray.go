fd87411ab69d9f07a9cdd16800948155e3113211
silentFellow <gowtham.sri+github@zohomail.in>
2025-02-27 09:56:29 +0530

    Update files in 1749-Maximum-Absolute-Sum-of-Any-Subarray

--- 1749-Maximum-Absolute-Sum-of-Any-Subarray/1749-Maximum-Absolute-Sum-of-Any-Subarray.go
+++ 1749-Maximum-Absolute-Sum-of-Any-Subarray/1749-Maximum-Absolute-Sum-of-Any-Subarray.go
@@ -1,24 +1,28 @@
 package leetcode
 
+// INTITUTION: use 2 kandine's algorithm
+// one for min-sum subarray and another for max-sum subarray
+// return max(abs(minSum), abs(maxSum))
+
 func maxAbsoluteSum(nums []int) int {
 	maxKandine, minKandine, maxSum, minSum := 0, 0, 0, 0
 
 	for _, v := range nums {
-    maxSum += v
-    minSum += v
+		maxSum += v
+		minSum += v
 
 		if maxSum < 0 {
 			maxSum = 0
 		}
-    if minSum > 0 {
-      minSum = 0
-    }
+		if minSum > 0 {
+			minSum = 0
+		}
 
 		maxKandine = max(maxKandine, maxSum)
 		minKandine = min(minKandine, minSum)
 	}
 
-  return max(abs(maxKandine), abs(minKandine))
+	return max(abs(maxKandine), abs(minKandine))
 }
 
 func abs(v int) int {
