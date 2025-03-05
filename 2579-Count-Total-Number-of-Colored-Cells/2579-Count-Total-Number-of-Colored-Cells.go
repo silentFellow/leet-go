5bf8d0356061619465d2e2d13fc37be9e06c2233
silentFellow <gowtham.sri+github@zohomail.in>
2025-03-05 11:37:27 +0530

    Update files in 2579-Count-Total-Number-of-Colored-Cells

--- 2579-Count-Total-Number-of-Colored-Cells/2579-Count-Total-Number-of-Colored-Cells.go
+++ 2579-Count-Total-Number-of-Colored-Cells/2579-Count-Total-Number-of-Colored-Cells.go
@@ -1,7 +1,8 @@
 package leetcode
 
 // NOTE: optimal solution:
-// return int64(1 + n * (n-1) * 2)
+// 1. return int64(1 + n * (n-1) * 2)
+// 2. return int64(math.Pow(float64(n), 2) + math.Pow(float64(n-1), 2))
 
 func coloredCells(n int) int64 {
 	ans := 1
