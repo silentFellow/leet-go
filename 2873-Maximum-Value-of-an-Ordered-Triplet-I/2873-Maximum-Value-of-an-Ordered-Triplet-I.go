347234664df58bf19910c82f05ed155b013006ba
silentFellow <gowtham.sri+github@zohomail.in>
2025-04-04 22:32:12 +0530

    Add new files in 2873-Maximum-Value-of-an-Ordered-Triplet-I

---
+++ 2873-Maximum-Value-of-an-Ordered-Triplet-I/2873-Maximum-Value-of-an-Ordered-Triplet-I.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import "math"
+
+func maximumTripletValue(nums []int) int64 {
+	n := len(nums)
+
+	prefixMax := make([]int, len(nums))
+	maxv := math.MinInt
+	for i, v := range nums {
+		maxv = max(maxv, v)
+		prefixMax[i] = maxv
+	}
+
+	suffixMax := make([]int, len(nums))
+	maxv = math.MinInt
+	for i := n - 1; i >= 0; i-- {
+		maxv = max(maxv, nums[i])
+		suffixMax[i] = maxv
+	}
+
+	ans := 0
+	for i := 1; i < n-1; i++ {
+		ans = max(ans, (prefixMax[i-1]-nums[i])*suffixMax[i+1])
+	}
+
+	return int64(ans)
+}
