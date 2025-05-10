daea4963d4683a5a572bb0aa164fe732b17d3a06
silentFellow <gowtham.sri+github@zohomail.in>
2025-05-10 17:12:12 +0530

    Update files in 2918-Minimum-Equal-Sum-of-Two-Arrays-After-Replacing-Zeros

--- 2918-Minimum-Equal-Sum-of-Two-Arrays-After-Replacing-Zeros/2918-Minimum-Equal-Sum-of-Two-Arrays-After-Replacing-Zeros.go
+++ 2918-Minimum-Equal-Sum-of-Two-Arrays-After-Replacing-Zeros/2918-Minimum-Equal-Sum-of-Two-Arrays-After-Replacing-Zeros.go
@@ -21,9 +21,5 @@ func minSum(nums1 []int, nums2 []int) int64 {
 		return -1
 	}
 
-	if (s1 + z1) > (s2 + z2) {
-		return int64(s1 + z1)
-	} else {
-		return int64(s2 + z2)
-	}
+	return int64(max(s1+z1, s2+z2))
 }
