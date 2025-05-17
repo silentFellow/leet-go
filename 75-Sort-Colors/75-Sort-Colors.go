ca9a1c103c7e74f6de6e061f2d9540fdd915000f
silentFellow <gowtham.sri+github@zohomail.in>
2025-05-17 18:59:27 +0530

    Update files in 75-Sort-Colors

--- 75-Sort-Colors/75-Sort-Colors.go
+++ 75-Sort-Colors/75-Sort-Colors.go
@@ -24,3 +24,22 @@ func sortColors(nums []int) {
 		}
 	}
 }
+
+// one pass algorithm
+// func sortColors(nums []int) {
+// 	l, m, r := 0, 0, len(nums)-1
+//
+// 	for m <= r {
+// 		if nums[m] == 0 {
+// 			nums[l], nums[m] = nums[m], nums[l]
+// 			l++
+// 			m++
+// 		} else if nums[m] == 1 {
+// 			m++
+// 		} else {
+// 			nums[r], nums[m] = nums[m], nums[r]
+// 			r--
+// 			m++
+// 		}
+// 	}
+// }
