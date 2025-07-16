f9e85a34b975e5a38b3463e588662e5dd67add8a
silentFellow <gowtham.sri+github@zohomail.in>
2025-07-16 14:12:20 +0530

    Update files in 1-Two-Sum

--- 1-Two-Sum/1-Two-Sum.go
+++ 1-Two-Sum/1-Two-Sum.go
@@ -1,17 +1,17 @@
 package leetcode
 
 func twoSum(nums []int, target int) []int {
-  hmap := make(map[int]int)
+	hmap := make(map[int]int)
 
-  for i, val := range nums {
-    req := target-val
-    existIndex, exists := hmap[req]
-    if exists {
-      return []int{ existIndex, i }
-    }
+	for i, val := range nums {
+		req := target - val
+		existIndex, exists := hmap[req]
+		if exists {
+			return []int{existIndex, i}
+		}
 
-    hmap[val] = i
-  }
+		hmap[val] = i
+	}
 
-  return []int{-1, -1}
+	return []int{-1, -1}
 }
